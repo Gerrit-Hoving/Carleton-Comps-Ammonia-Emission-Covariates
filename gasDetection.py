490c2efde56dc1e3f72ada2668b2a3e6a1a99119
Gerrit Hoving <ghoving@protonmail.com>
2025-01-27 08:15:11 -0600

    Added code to visualize spectra

--- gasDetection.py
+++ gasDetection.py
@@ -14,12 +14,19 @@ from osgeo import gdal
 import rasterio
 from scipy.linalg import norm, inv
 
+from skimage import exposure
+import seaborn as sns
+import hvplot
+import hvplot.xarray
+from emit_tools import emit_xarray
+import pandas as pd
+
 
 db_begin('../data/HITRAN')
 
-#fetch('NH3',11,1,4000,27000)
+fetch('CH4',6,1,4000,27000)
 
-nu,coef = hapi.absorptionCoefficient_Lorentz(SourceTables='NH3', Diluent={'air':1.0}, Environment = {'p':0.98,'T':305},)
+nu,coef = hapi.absorptionCoefficient_Lorentz(SourceTables='CH4', Diluent={'air':1.0}, Environment = {'p':0.98,'T':305},)
 
 plt.plot(nu,coef)
 
@@ -92,11 +99,11 @@ for row in range(raster_data.shape[1]):
         spectrum /= np.max(spectrum)
         
         # Perform the matched filter - dot product between the spectrum and the absorption coefficients
-        matched_filter = np.dot(spectrum, coef)  # We assume both spectrum and coef have the same shape
-        #matched_filter = ((spectrum-mu).dot(Cinv.dot(coef)))/(coef.dot(Cinv.dot(coef)))
+        #matched_filter = np.dot(spectrum, coef)  # We assume both spectrum and coef have the same shape
+        matched_filter = ((spectrum-mu).dot(Cinv.dot(coef)))/(coef.dot(Cinv.dot(coef)))
         
         
-        matched_filter = matched_filter if matched_filter > (10^(-20)) else 0
+        #matched_filter = matched_filter if matched_filter > (10^(-20)) else 0
         
         # Calculate concentration (you can modify this calculation based on the Beer-Lambert Law)
         # Simplified: assuming direct proportionality between matched filter and concentration
@@ -108,16 +115,16 @@ for row in range(raster_data.shape[1]):
 #### Matched filter implementation from isofit tutorials
 rows, cols, bands = 2018,2239,285
 
-mm = np.memmap(raster_file, dtype=np.float32, mode='r', shape=(rows, cols, bands))
-#mm = raster_data
+#mm = np.memmap(raster_file, dtype=np.float32, mode='r', shape=(rows, cols, bands))
+mm = raster_data
 X = np.asarray(mm).copy()
 
-X = X.reshape(rows, cols, bands)
+X = X.reshape(rows*cols, bands)
 
 # A subset of pixels is sufficient, say one out of every 100
 subset = np.arange(0,X.shape[0],100)
 Xsub = X[subset,:]
-#Xsub = Xsub[~np.isnan(Xsub).any(axis=1)]
+Xsub = Xsub[~np.isnan(Xsub).any(axis=1)]
 mu = Xsub.mean(axis=0)
 
 # Calculate the covariance
@@ -126,8 +133,9 @@ Cov = np.cov(Xsub, rowvar=False);
 Cinv = inv(Cov + np.eye(len(nu))*1e-8)
 mf = ((X-mu).dot(Cinv.dot(coef)))/(coef.dot(Cinv.dot(coef)))
 
-mf = mf.reshape(rows, cols)
-plt.imshow(mf * 10000.0)
+## TODO: Fix reshape to get comprehensible output
+out = mf.reshape(cols, rows, 1)
+plt.imshow(out * 10000.0)
 plt.colorbar()
 plt.clim([0,200])
 plt.title('NH3 plume (ppm m)')
@@ -144,9 +152,11 @@ plt.title('Concentration Map')
 plt.savefig('interpolated_absorption_coefficients.tif', format='tiff', dpi=800)
 plt.show()
 
-
-threshold = 8e-21
-new_array = np.where(concentration_map < threshold, 0, concentration_map)
+#concentration_map = abs(concentration_map) / 1e17
+low = 0
+high = 2
+new_array = np.where(concentration_map < low, low, concentration_map)
+new_array = np.where(new_array > high, low, new_array)
 plt.figure()
 plt.imshow(new_array, cmap='viridis')
 plt.colorbar(label='Concentration')
@@ -163,6 +173,112 @@ meta.update({
     'count': 1,  # Single band (grayscale image)
     'dtype': 'float32',  # Data type for concentration map
 })
-with rasterio.open('concentration_map.tif', 'w', **meta) as dst:
+with rasterio.open('concentration_map_ch4.tif', 'w', **meta) as dst:
     dst.write(concentration_map.astype('float32'), 1)  # Write the data to the first band
+
+
+
+
+### Visualize in/out of plume spectra
+
+
+
+rad = emit_xarray(raster_file, ortho=True)
+
+#In manure pond
+#points = pd.DataFrame([{"ID": 0, "latitude": 35.167664437574814, "longitude": -119.10087986567486}, {"ID": 1, "latitude": 35.1676775784815, "longitude": -119.09837215132448}])
+# In hills
+points = pd.DataFrame([{"ID": 0, "latitude": 35.31848809204527, "longitude": -119.42502781410386}, {"ID": 1, "latitude": 35.31897748845836, "longitude": -119.42502800538341}])
+#Feedlot surface, accounting for wind
+#points = pd.DataFrame([{"ID": 0, "latitude": 35.16709534468988, "longitude": -119.09588385907719}, {"ID": 1, "latitude": 35.17023191518769, "longitude": -119.09584450552866}])
+#Feedlot surface 2, accounting for wind
+#points = pd.DataFrame([{"ID": 0, "latitude": 35.17118076215722, "longitude": -119.10268430272738}, {"ID": 1, "latitude": 35.17202346329284, "longitude": -119.10853490784534}])
+#Western Sky Dairy
+#points = pd.DataFrame([{"ID": 0, "latitude": 35.18950382181828, "longitude": -119.11785339563848}, {"ID": 1, "latitude": 35.19335877937062, "longitude": -119.11997608881786}])
+#T&W Farms
+#points = pd.DataFrame([{"ID": 0, "latitude": 35.191961378381194, "longitude": -119.10283239289654}, {"ID": 1, "latitude": 35.19367198662642, "longitude": -119.10701881555586}])
+
+
+points = points.set_index(['ID'])
+
+point_ds = rad.sel(latitude=points.to_xarray().latitude, longitude=points.to_xarray().longitude, method='nearest')
+
+
+# Create an RGB from Radiance
+rgb = rad.sel(wavelengths=[650,560,470], method='nearest')
+rgb.radiance.data[rgb.radiance.data == -9999] = 0
+rgb.radiance.data = exposure.rescale_intensity(rgb.radiance.data, in_range='image', out_range=(0,1))
+
+
+
+
+df = point_ds.to_dataframe().reset_index()
+
+# Plot using seaborn
+plt.figure(figsize=(8, 6))  # Equivalent to frame_height=400, frame_width=600
+
+# Create the line plot with seaborn
+sns.lineplot(
+    data=df,
+    x='wavelengths',
+    y='radiance',
+    hue='ID',  # Group data by 'ID'
+    palette='Dark2',  # Use Dark2 color palette
+    marker='o'  # Optional: add markers to lines
+)
+
+# Add titles and labels
+plt.title('Radiance Spectra, ID 0 is in-plume')
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Radiance (W/m^2/sr/nm)')
+
+# Show the plot
+plt.show()
+
+
+in_plume = df.loc[df['ID'] == 0].copy()
+out_plume = df.loc[df['ID'] == 1].copy()
+out_plume['band_ratio'] = (in_plume.loc[0, 'radiance']/out_plume['radiance'])
+
+
+# Plot using seaborn
+plt.figure(figsize=(8, 6))  # Equivalent to frame_height=400, frame_width=600
+
+# Create the line plot with seaborn
+sns.lineplot(
+    data=out_plume,
+    x='wavelengths',
+    y='band_ratio',
+    hue='ID',  # Group data by 'ID'
+    palette='Dark2',  # Use Dark2 color palette
+    marker='o'  # Optional: add markers to lines
+)
+
+# Add titles and labels
+plt.title('In Plume/Out of Plume Ratio - Hills')
+plt.xlabel('Wavelength (nm)')
+plt.xlim(1700, 2400)
+plt.ylabel('In Plume/Out of Plume Ratio')
+plt.ylim(0, 100)
+
+"""
+for i in range(498, 510, 1):
+    plt.text(out_plume['wavelengths'][i], 
+             out_plume['band_ratio'][i], 
+             str(i) + ':' + str(out_plume['wavelengths'][i]), 
+             fontsize=6, ha='right', va='bottom')
+
+"""
+
+# Show the plot
+plt.show()
+
+
+
+
+
+
+
+
+
 
